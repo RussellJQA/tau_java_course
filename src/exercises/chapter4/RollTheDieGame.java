9d2bed6bac77e7f4a3727f48b55c93cc9aacc5bd
Russell Johnson <16086475+RussellJQA@users.noreply.github.com>
2021-05-17 23:54:50 -0400

    More for chapter 4 (loops)

--- src/exercises/chapter4/RollTheDieGame.java
+++ src/exercises/chapter4/RollTheDieGame.java
@@ -10,24 +10,34 @@ public class RollTheDieGame {
         Scanner scanner = new Scanner(System.in);
         Random r = new Random();
 
-        int max_rolls = 5;
-        int board_size = 20;
-        int board_loc = 0;
+        int maxRolls = 5;
+        int boardSize = 20;
+        int boardLoc = 0;
 
-        for (int i = 0; i < max_rolls; i++) {
+        String moreToGoMsg;
+
+        for (int i = 0; i < maxRolls; i++) {
             int die = r.nextInt(6) + 1;
-            if(board_loc + die > board_size) {
-                System.out.println("Roll #" + (i+1) + ": You've rolled a " + die + ", but there are only " + (board_size-board_loc) + " more spaces to go.");
+            String rolledMsg = "Roll #" + (i+1) + ": You've rolled a " + die + ".";
+            if(boardLoc + die > boardSize) {
+                int moreToGo = boardSize-boardLoc;
+                if(moreToGo == 1)
+                    moreToGoMsg = "is only 1 more space";
+                else
+                    moreToGoMsg = "are only " + moreToGo + " more spaces";
+                System.out.println(rolledMsg + " But you were already on space " + boardLoc + ", and there " + moreToGoMsg + " beyond that.");
                 break;
             }
-            board_loc += die;
-            if(board_loc == board_size)
-                System.out.println("Roll #" + (i+1) + ": You've rolled a " + die + ". You're on space " + board_loc + ". Congrats, you win!");
-            else
-                System.out.println("Roll #" + (i+1) + ": You've rolled a " + die + ". You are now on space " + board_loc + " and have " + (board_size-board_loc) + " more to go.");
+            boardLoc += die;
+            if(boardLoc == boardSize) {
+                System.out.println(rolledMsg + " You are now on space " + boardLoc + ". Congrats, you win!");
+                break;
+            } else
+                System.out.println(rolledMsg + " You are now on space " + boardLoc + " and have " + (boardSize-boardLoc) + " more to go.");
         }
-        if(board_loc != board_size)
-            System.out.println("Game over! You lose!");
+
+        if(boardLoc != boardSize)
+            System.out.println("You lose!");
 
         scanner.close();
     }
